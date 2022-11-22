6bf96140efc6b48351d7e89179f6d210dad74ca3
Jason Heppler <jason.heppler@gmail.com>
2022-11-22 11:00:17 -0600

    Implement zoom. Testing jittering points.

--- assets/map.js
+++ assets/map.js
@@ -8,8 +8,51 @@ export default class HearingMap extends Visualization {
       .geoMercator()
       .translate([this.width / 2, this.height / 2])
       .center([-82.366, 23.113])
-      .scale(600);
+      .scale(400);
     this.path = d3.geoPath().projection(this.projection);
+
+    this.kScale = 0.5;
+    this.centered = null;
+
+    this.jitter = 5;
+
+    this.zoom = (e, d) => {
+      let x, y, k;
+      if (d && this.centered !== d) {
+        const coords = this.projection([d.lon, d.lat]);
+        [x, y] = coords;
+        k = 10;
+        this.kScale = 4;
+        this.centered = d;
+      } else {
+        x = this.width / 2;
+        y = this.height / 2;
+        k = 1;
+        this.kScale = 1;
+        this.centered = null;
+      }
+
+      this.viz
+        .transition()
+        .duration(750)
+        .attr(
+          "transform",
+          `translate(${this.width / 2},${this.height / 2})
+          scale(${k}) translate(${-x},${-y})`
+        );
+      
+      this.viz 
+          .selectAll(".country")
+          .transition()
+          .duration(750)
+          .attr("stroke-width", `${0.5 / this.kScale}px`);
+      
+      this.viz
+          .selectAll(".point")
+          .transition()
+          .duration(750)
+          .attr("stroke-width", `${1 / this.kScale}px`);
+    }
   }
 
   render() {
@@ -46,6 +89,13 @@ export default class HearingMap extends Visualization {
       .append("path")
       .attr("d", this.path)
       .attr("class", "country");
+    
+      this.viz
+      .append("rect")
+      .attr("class", "overlay")
+      .attr("width", this.width)
+      .attr("height", this.height)
+      .on("click", this.zoom);
   }
 
   update() {
@@ -69,12 +119,16 @@ export default class HearingMap extends Visualization {
         .data(recordings)
         .enter()
         .append("circle")
-        // TODO: if the points overlap (i.e., cx == cx), jitter them
-        .attr("cx", (d) => this.projection([d.lon, d.lat])[0])
-        .attr("cy", (d) => this.projection([d.lon, d.lat])[1])
-        .attr("r", (d) => Math.sqrt(d.recordings))
+        .attr("cx", (d) => this.projection([d.lon, d.lat])[0] - (Math.random() * this.jitter) )
+        .attr("cy", (d) => this.projection([d.lon, d.lat])[1] - (Math.random() * this.jitter) )
+        .attr("r", (d) => Math.sqrt(d.recordings / Math.PI))
         .classed("point", true)
         .attr("stroke-width", 0.5);
+      
+      // Zoom to the country when clicked and adjust the stroke width of the circle.
+      this.viz
+      .selectAll("circle")
+      .on("click", (e, d) => this.zoom(e, d));
     });
   }
 }
