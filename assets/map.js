38289ebc0e7c7315773d2e3ec808887c55f0b634
Jason Heppler <jason.heppler@gmail.com>
2022-11-28 15:02:48 -0600

    Adding dropdown and range slider watchers.

--- assets/map.js
+++ assets/map.js
@@ -8,13 +8,17 @@ export default class HearingMap extends Visualization {
       .geoMercator()
       .translate([this.width / 2, this.height / 2])
       .center([-82.366, 6.113])
-      .scale(400);
+      .scale(480);
     this.path = d3.geoPath().projection(this.projection);
 
     this.kScale = 0.5;
     this.centered = null;
 
     this.jitter = 5;
+    this.timer = null;
+
+    this.playButton = document.getElementById("play-timeline");
+    this.resetButton = document.getElementById("reset-timeline");
 
     this.zoom = (e, d) => {
       let x, y, k;
@@ -40,23 +44,22 @@ export default class HearingMap extends Visualization {
           `translate(${this.width / 2},${this.height / 2})
           scale(${k}) translate(${-x},${-y})`
         );
-      
-      this.viz 
-          .selectAll(".country")
-          .transition()
-          .duration(750)
-          .attr("stroke-width", `${0.5 / this.kScale}px`);
-      
+
       this.viz
-          .selectAll(".point")
-          .transition()
-          .duration(750)
-          .attr("stroke-width", `${1 / this.kScale}px`);
-    }
+        .selectAll(".country")
+        .transition()
+        .duration(750)
+        .attr("stroke-width", `${0.5 / this.kScale}px`);
+
+      this.viz
+        .selectAll(".point")
+        .transition()
+        .duration(750)
+        .attr("stroke-width", `${1 / this.kScale}px`);
+    };
   }
 
   render() {
-    // Color the map water features
     this.viz
       .append("path")
       .datum(this.data.northamerica)
@@ -64,15 +67,15 @@ export default class HearingMap extends Visualization {
       .attr("fill", "#F0F0F4")
       .attr("stroke", "#000000")
       .attr("stroke-width", 0.5);
-    
-      this.viz
+
+    this.viz
       .append("path")
       .datum(this.data.southamerica)
       .attr("d", this.path)
       .attr("fill", "#F0F0F4")
       .attr("stroke", "#000000")
       .attr("stroke-width", 0.5);
-    
+
     // Draw the map features
     this.viz
       .selectAll("path")
@@ -81,16 +84,16 @@ export default class HearingMap extends Visualization {
       .append("path")
       .attr("d", this.path)
       .attr("class", "country");
-    
-      this.viz
+
+    this.viz
       .selectAll("path")
       .data(this.data.southamerica.features)
       .enter()
       .append("path")
       .attr("d", this.path)
       .attr("class", "country");
-    
-      this.viz
+
+    this.viz
       .append("rect")
       .attr("class", "overlay")
       .attr("width", this.width)
@@ -104,13 +107,57 @@ export default class HearingMap extends Visualization {
       // Create a new array of objects
       const recordings = data.map((d) => {
         return {
-          id: d.id,
-          date: d.date,
+          start_date: d.start_date,
+          end_date: d.end_date,
           country: d.country,
           lat: +d.lat,
           lon: +d.lon,
-          recordings: +d.recordings
+          scouts: d.scouts,
+          recordings: +d.recordings,
         };
+      });
+
+      // We create our array of scouts from recordings.csv to use in the dropdown.
+      const scouts = [];
+      recordings.forEach((d) => {
+        scouts.push(d.scouts);
+      });
+      // We then separate scouts by delimiter, return a unique array of scouts, and sort.
+      const uniqueScouts = scouts
+        .join(";")
+        .split(";")
+        .filter((d, i, a) => a.indexOf(d) === i)
+        .sort();
+      // Trim white space from each scout name
+      const trimmedScouts = uniqueScouts.map((d) => d.trim());
+      // We then add the "All" option to the top of the array.
+      trimmedScouts.unshift("All");
+
+      // Create a dropdown menu
+      d3.select("#scouts-dropdown")
+        .append("label")
+        .text("Select a scout")
+        .append("select")
+        .attr("id", "scouts_selection")
+        .selectAll("option")
+        .data(trimmedScouts)
+        .join("option")
+        .attr("value", (d) => d)
+        .text((d) => d);
+
+      d3.select("#map__dropdown_scouts--select")
+        .selectAll("option")
+        .data(trimmedScouts)
+        .enter()
+        .append("option")
+        .attr("value", (d) => d)
+        .text((d) => d);
+
+      // Retrieve the value of the range slider
+      const slider = document.getElementById("timeline");
+      slider.addEventListener("change", () => {
+        const year = slider.valueAsNumber;
+        console.log(year);
       });
 
       // Draw the circles
@@ -119,16 +166,27 @@ export default class HearingMap extends Visualization {
         .data(recordings)
         .enter()
         .append("circle")
-        .attr("cx", (d) => this.projection([d.lon, d.lat])[0] - (Math.random() * this.jitter) )
-        .attr("cy", (d) => this.projection([d.lon, d.lat])[1] - (Math.random() * this.jitter) )
+        .attr(
+          "cx",
+          (d) =>
+            this.projection([d.lon, d.lat])[0] - Math.random() * this.jitter
+        )
+        .attr(
+          "cy",
+          (d) =>
+            this.projection([d.lon, d.lat])[1] - Math.random() * this.jitter
+        )
         .attr("r", (d) => Math.sqrt(d.recordings / Math.PI))
         .classed("point", true)
         .attr("stroke-width", 0.5);
-      
+
       // Zoom to the country when clicked and adjust the stroke width of the circle.
-      this.viz
-      .selectAll("circle")
-      .on("click", (e, d) => this.zoom(e, d));
+      this.viz.selectAll("circle").on("click", (e, d) => this.zoom(e, d));
+
+      // If the reset button is pressed, reset to the timeline-label to 1910-1932
+      this.resetButton.addEventListener("click", () => {
+        document.getElementById("year-range").innerHTML = "1910-1932";
+      });
     });
   }
 }
