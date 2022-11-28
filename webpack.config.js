1107f4cd142f9cc9b7b8e3e79c1222fe740ea4e7
Jason Heppler <jason.heppler@gmail.com>
2022-11-28 11:19:31 -0600

    Minify CSS with node.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,11 @@
 const webpack = require('webpack');
 const path = require('path');
 
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const fs = require('fs');
+
 // We bundle assets/main.js as well as the files it imports. We also bundle the
 // files that main.js imports, and so on. This is called "tree shaking".
 // We use the "mode" option to tell webpack to use its built-in
@@ -19,3 +24,13 @@ module.exports = {
         }),
     ],
 };
+
+const minifyCss = async () => {
+    const output = await postcss([cssnano, autoprefixer]).process(
+        fs.readFileSync('assets/main.css', 'utf8'),
+        { from: 'assets/main.css', to: 'dist/main.css' }
+    );
+    fs.writeFileSync('dist/main.css', output.css);
+};
+
+minifyCss();
