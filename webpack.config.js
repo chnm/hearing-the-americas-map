d78d42a86ef70b7b6d4e82059d28f4fcbb0859d7
Jason Heppler <jason.heppler@gmail.com>
2023-02-09 13:37:31 -0600

    Better prod and dev handling

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,34 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const fs = require('fs');
 
+// For development, we want to bundle the files together, but we don't want
+// to minify them. We also want to use source maps so that we can debug
+// our original source files.
+module.exports = {
+    mode: 'development',
+    entry: './src/assets/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(csv)$/,
+                loader: 'csv-loader',
+                options: {
+                    header: true
+                }
+            },
+        ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            d3: 'd3',
+        }),
+    ],
+};
+
 // We bundle assets/main.js as well as the files it imports. We also bundle the
 // files that main.js imports, and so on. This is called "tree shaking".
 // We use the "mode" option to tell webpack to use its built-in
